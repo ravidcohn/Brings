5fce65fb63d17d73010f623a5a5379ba21b84929
ravid_cohen <ravidcohn@gmail.com>
2015-09-09 14:56:45 +0300

    fucking shit

--- app/src/main/java/app/some_lie/brings/newEvent.java
+++ app/src/main/java/app/some_lie/brings/newEvent.java
@@ -2,18 +2,34 @@ package app.some_lie.brings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 /**
  * Created by pinhas on 08/09/2015.
  */
 public class newEvent extends Activity {
 
+    private TextView tv_ne_name_ui;
+    private TextView tv_ne_place_ui;
+    private TextView tv_ne_start_ui;
+    private TextView tv_ne_end_ui;
+    private TextView tv_ne_description_ui;
+    private Button bt_ne_pic_ui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_event);
+        tv_ne_name_ui = (TextView)findViewById(R.id.tv_ne_name_ui);
+        tv_ne_place_ui = (TextView)findViewById(R.id.tv_ne_place_ui);
+        tv_ne_start_ui = (TextView)findViewById(R.id.tv_ne_start_ui);
+        tv_ne_end_ui = (TextView)findViewById(R.id.tv_ne_end_ui);
+        tv_ne_description_ui = (TextView)findViewById(R.id.tv_ne_description_ui);
 
+        tv_ne_description_ui = (Button)findViewById(R.id.bt_ne_pic_ui);
     }
 
 
